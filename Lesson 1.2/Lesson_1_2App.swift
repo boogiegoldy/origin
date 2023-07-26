937f0336eb33fbd2c5595902bc63ce39d03d200a
Eva <evaztan10@gmail.com>
2023-07-26 13:37:40 -0700

    Initial Commit

---
+++ Lesson 1.2/Lesson_1_2App.swift
@@ -0,0 +1,17 @@
+//
+//  Lesson_1_2App.swift
+//  Lesson 1.2
+//
+//  Created by Eva Tan on 2023-07-26.
+//
+
+import SwiftUI
+
+@main
+struct Lesson_1_2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
